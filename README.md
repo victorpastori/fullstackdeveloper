4851eb8389ffe7baf0967bdc16a445762226b2e0
marciogervazoni <marciogervazoni@gaudium.com.br>
2020-09-16 16:53:46 -0300

    Configuração Inicial do Ambiente

---
+++ README.md
@@ -0,0 +1,41 @@
+# Gaudium Full Stack Developer Exam
+
+Bem vindo(a) ao ambiente de desenvolvimento do processo seletivo para Desenvolvedor Full Stack da Gaudium!
+
+Este ambiente será utilizado durante a prova prática do processo.
+
+Faça um fork deste repositório para um novo repositório na sua conta do Github e siga as instruções abaixo.
+
+## Verificação do ambiente
+
+É necessário ter a versão estável mais recente do [Docker Desktop](https://www.docker.com/products/docker-desktop) instalada no seu computador.
+
+Uma vez que o Docker Desktop esteja funcionando no seu computador, você deve:
+
+1. Baixar o seu novo repositório para o seu computador, usando o o comando `git clone` ou um cliente git como o [GitHub Desktop](https://desktop.github.com/).
+
+2. Verificar o funcionamento do ambiente. Para isto, abra um terminal de linha de comandos na pasta `gdfs-workspace` e digite o comando 
+
+   `docker-compose up -d`
+
+3. Após verificar que os containers foram ativados, abra o seu navegador no endereço
+
+    [http://127.0.0.1:8123]( http://127.0.0.1:8123)
+
+   Uma página com a mensagem **Ambiente instalado com sucesso!** deverá ser exibida.
+
+## Estrutura do ambiente
+
+- A pasta `www` conterá os arquivos php, html, javascript, css e imagens que você produzirá durante a prova prática.
+- O servidor web responderá no endereço `127.0.0.1:8123` 
+- O banco de dados MySQL 5.6 responderá na porta `8456`
+- Os dados de conexão ao banco podem ser encontrados no código do script `index.php`.
+- A pasta `db` (que será criada quando o ambiente for ativado) conterá o banco de dados e deve ser comitada também durante a prova. Você não deve alterar diretamente o conteúdo desta pasta.
+- A pasta `docker` contém as imagens docker do Nginx, PHP e MySQL que compõem o ambiente. Não faça alterações nesta pasta.
+
+## Ferramentas recomendadas
+
+Durante a prova, você precisará acessar o banco de dados MySQL para criar tabelas e fazer outras consultas. Sugerimos que use um programa como o [SequelPro](https://sequelpro.com/) (para Mac) ou [HeidiSQL](https://www.heidisql.com/) (para Windows) para isto.
+
+Você também deve escolher um editor de código com que esteja habituado. Na Gaudium, nós usamos o [VSCode](https://code.visualstudio.com/) e o [PHPStorm](https://www.jetbrains.com/pt-br/phpstorm/).
+
---
+++ gdfs-workspace/www/index.php
@@ -0,0 +1,65 @@
+<?php
+
+$dsn = 'mysql:host=gd-fs-docker-mysql;port=3306;dbname=gdfs';
+$user = 'gdfs';
+$password = 'gdsecret';
+
+$db = null;
+
+$ok = true;
+
+try {
+	$db = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+	
+	$sql = "select curdate() from dual";
+	
+    $statement = $db->prepare($sql);
+
+    $now = $statement->fetchColumn();
+
+    $ok = ($now === FALSE);
+			
+} catch (PDOException $e) {
+	$ok = false;
+}
+
+?>
+<!doctype html>
+<html lang="pt_BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Gaudium Software - Prova Desenvolvedor Full Stack</title>
+  </head>
+  <body>  
+    <main role="main">
+      <div class="container mx-auto">
+        <div class="row">
+          <h1 class="mx-auto"><img src="assets/gaudium-logo.png" alt="Gaudium logo" width=100 /> Gaudium Software</h1>
+        </div>
+        <div class="row">   
+          <h2 class="mx-auto">Prova Desenvolvedor Full Stack</h2>       
+        </div>   
+        <div class="row pt-5">   
+          <div class="jumbotron mx-auto">
+          
+          
+          <?php if ($ok) { ?>        
+          <div class="alert alert-success mx-auto" role="alert">
+            Ambiente instalado com sucesso!
+          </div>
+          <?php } else { ?>
+          <div class="alert alert-danger mx-auto" role="alert">
+            Não foi possível conectar ao banco de dados.
+          </div>
+          <?php } ?>
+          </div>
+        </div>
+      </div> <!-- /container -->
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
